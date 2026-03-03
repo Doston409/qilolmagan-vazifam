e22ef1aabd8340412d64e65f42309c91d692f895
Пользователь <dabdugapparov806@gmail.com>
2026-03-03 23:13:26 +0500

    nam

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include,views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page, name='home')   
+    path('', views.home_page, name='home')
 ]
 

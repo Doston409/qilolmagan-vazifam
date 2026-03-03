299e090720aeff8b9caa849a9b5575e0f53f7699
Пользователь <dabdugapparov806@gmail.com>
2026-03-03 23:27:01 +0500

    nam

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from . import views  # bu sizning views.py dan oladi
 
 urlpatterns = [
-    path('', views.home_page, name='home')
+    path('', views.home_page, name='home'),  # home_page view chaqiriladi
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,5 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-
 def home_page(request):
-    return render(request, "index.html")
-
+    return render(request, 'index.html')

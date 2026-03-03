2db9725db53e13b8b87b22ea46f7cabac4d99d32
Пользователь <dabdugapparov806@gmail.com>
2026-03-03 23:28:47 +0500

    nam

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # bu sizning views.py dan oladi
 
 urlpatterns = [
-    path('', views.home_page, name='home'),  # home_page view chaqiriladi
+    path('', views.index_page, name='index'),  # home_page view chaqiriladi
 ]
